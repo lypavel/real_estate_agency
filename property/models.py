d4fcba9859f79da56842957a4b8718d27fc13bce
lypavel <157053921+lypavel@users.noreply.github.com>
2024-03-19 11:59:58 +0300

    Add model field Flat.new_building

--- property/models.py
+++ property/models.py
@@ -9,6 +9,11 @@ class Flat(models.Model):
         'Когда создано объявление',
         default=timezone.now,
         db_index=True)
+    new_building = models.BooleanField(
+        'Новостройка',
+        null=True,
+        blank=True
+    )
 
     description = models.TextField('Текст объявления', blank=True)
     price = models.IntegerField('Цена квартиры', db_index=True)
